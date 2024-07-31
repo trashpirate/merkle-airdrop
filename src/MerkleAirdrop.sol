dbdf1edd904680d5828db14c6787ce4b9034846d
trashpirate <nadinaoates@gmail.com>
2024-07-31 14:49:35 -0500

    implementing signature

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -3,13 +3,25 @@ pragma solidity ^0.8.20;
 
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-contract MerkleAirdrop {
+contract MerkleAirdrop is EIP712 {
     using SafeERC20 for IERC20;
+
+    /**
+     * TYPES
+     */
+    struct AirdropClaim {
+        address account;
+        uint256 amount;
+    }
 
     /**
      * STORAGE VARIABLES
      */
+    bytes32 private constant MESSAGE_TYPEHASH = keccak256("AirdropClaim(address account, uint256 amount)");
+
     bytes32 private immutable i_merkleRoot;
     IERC20 private immutable i_airdropToken;
 
@@ -27,7 +39,7 @@ contract MerkleAirdrop {
     error MerkleAirdrop__InvalidProof();
     error MerkleAirdrop__InvalidSignature();
 
-    constructor(bytes32 merkleRoot, address airdropToken) {
+    constructor(bytes32 merkleRoot, address airdropToken) EIP712("MerkleAirdrop", "1") {
         i_merkleRoot = merkleRoot;
         i_airdropToken = IERC20(airdropToken);
     }
@@ -55,11 +67,29 @@ contract MerkleAirdrop {
         i_airdropToken.safeTransfer(account, amount);
     }
 
+    // external functions
+
     function getMerkleRoot() external view returns (bytes32) {
         return i_merkleRoot;
     }
 
     function getAirdropToken() external view returns (address) {
         return address(i_airdropToken);
+    }
+
+    // public functions
+    function getMessage(address account, uint256 amount) public view returns (bytes32 digest) {
+        return
+            _hashTypedDataV4(keccak256(abi.encode(MESSAGE_TYPEHASH, AirdropClaim({account: account, amount: amount}))));
+    }
+
+    // private/internal functions
+    function _isValidSignature(address account, bytes32 digest, uint8 v, bytes32 r, bytes32 s)
+        internal
+        pure
+        returns (bool)
+    {
+        (address actualSigner,,) = ECDSA.tryRecover(digest, v, r, s);
+        return actualSigner == account;
     }
 }
--- test/MerkleAirdropTest.t.sol
+++ test/MerkleAirdropTest.t.sol
@@ -25,6 +25,7 @@ contract MerkleAirdropTest is Test {
         bytes32(0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576),
         bytes32(0xc2ef27e475b955c035da5b0bb2847aaadfab48761646acc36250572a3d56cbd3)
     ];
+    address gasPayer;
     address user;
     uint256 userPrivKey;
 
@@ -38,13 +39,19 @@ contract MerkleAirdropTest is Test {
 
         token.mint(address(airdrop), AMOUNT * 6);
         (user, userPrivKey) = makeAddrAndKey("user");
+        gasPayer = makeAddr("gasPayer");
     }
 
     function testUserCanClaim() public {
         uint256 startingBalance = token.balanceOf(user);
+        bytes32 digest = airdrop.getMessage(user, AMOUNT);
 
-        vm.prank(user);
-        airdrop.claim(user, AMOUNT, PROOF);
+        // sign message
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPrivKey, digest);
+
+        // claim
+        vm.prank(gasPayer);
+        airdrop.claim(user, AMOUNT, PROOF, v, r, s);
 
         uint256 endingBalance = token.balanceOf(user);
         console.log("ending balance ", endingBalance);
