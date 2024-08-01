0bb5215276a3c813f19bd348fdb145ad2de967ab
trashpirate <nadinaoates@gmail.com>
2024-08-01 13:37:10 -0500

    update readme

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -6,6 +6,9 @@ import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProo
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
+/// @title MerkleAirdrop
+/// @author Nadina Oates
+/// @notice Contract implementing a claiming contract using merkle proofs
 contract MerkleAirdrop is EIP712 {
     using SafeERC20 for IERC20;
 
@@ -39,11 +42,20 @@ contract MerkleAirdrop is EIP712 {
     error MerkleAirdrop__InvalidProof();
     error MerkleAirdrop__InvalidSignature();
 
+    /// @notice Constructor
+    /// @param merkleRoot Merkel root of the merkle tree
+    /// @param airdropToken Token address of token to be claimed
+    /// @dev inherits from Openzeppelin EIP712
     constructor(bytes32 merkleRoot, address airdropToken) EIP712("MerkleAirdrop", "1") {
         i_merkleRoot = merkleRoot;
         i_airdropToken = IERC20(airdropToken);
     }
 
+    /// @notice Claims airdrop tokens
+    /// @param account Address to receive airdrop
+    /// @param amount Amount to be claimed
+    /// @param merkleProof Merkle Proof associated with account
+    /// @param signature associated with account
     function claim(address account, uint256 amount, bytes32[] calldata merkleProof, uint8 v, bytes32 r, bytes32 s)
         external
     {
@@ -67,29 +79,50 @@ contract MerkleAirdrop is EIP712 {
         i_airdropToken.safeTransfer(account, amount);
     }
 
-    // external functions
+    /**
+     * EXTERNAL FUNCTIONS
+     */
 
+    /// @notice Gets merkle root
     function getMerkleRoot() external view returns (bytes32) {
         return i_merkleRoot;
     }
 
+    /// @notice Gets airdrop token address
     function getAirdropToken() external view returns (address) {
         return address(i_airdropToken);
     }
 
-    // public functions
+    /**
+     * PUBLIC FUNCTIONS
+     */
+    /// @notice Gets message hash for the airdrop
+    /// @param account Adress receiving the airdrop
+    /// @param amount Airdrop amount
     function getMessageHash(address account, uint256 amount) public view returns (bytes32 digest) {
         return
             _hashTypedDataV4(keccak256(abi.encode(MESSAGE_TYPEHASH, AirdropClaim({account: account, amount: amount}))));
     }
 
-    // private/internal functions
+    /// @notice Checks if signature is valid (for convenience, not use in contract)
+    /// @param account Adress receiving the airdrop
+    /// @param digest Message hash
+    /// @param signature Account's signature
     function isValidSignature(address account, bytes32 digest, bytes calldata signature) public pure returns (bool) {
         (address actualSigner,,) = ECDSA.tryRecover(digest, signature);
         return actualSigner == account;
     }
 
-    // private/internal functions
+    /**
+     * PRIVATE/INTERNAL
+     */
+
+    /// @notice Checks if signature is valid
+    /// @param account Adress receiving the airdrop
+    /// @param digest Message hash
+    /// @param v polarity
+    /// @param r x point on elliptic curve
+    /// @param s proof signer knows private key
     function _isValidSignature(address account, bytes32 digest, uint8 v, bytes32 r, bytes32 s)
         internal
         pure
